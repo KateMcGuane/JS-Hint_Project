de80743821341213ef2a771b36d3d0c572397f50
Kate McGuane <katemcguane@hotmail.com>
2024-06-06 10:14:40 +0000

    Add 3 constants to JS file

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const API_KEY = "tKkUkYnVJPlsQlYsktT084h76EE";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
