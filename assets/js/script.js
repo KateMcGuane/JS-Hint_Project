5ba3f622807b9c7f0aa7cf08715ef097d45f742c
Kate McGuane <katemcguane@hotmail.com>
2024-06-06 12:20:28 +0000

    Create displayErrors function with content & modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,49 @@
-const API_KEY = "tKkUkYnVJPlsQlYsktT084h76EE123";
+const API_KEY = "tKkUkYnVJPlsQlYsktT084h76EE";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 /* not actually using event listener for these lessons, but good habit to pass event into object handler */
 document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+                    "Authorization": API_KEY,
+                },
+        body: form,
+         /* will make a post request to the API, authorize with API & attach the form as the body of the request */
+        });
+        const data = await response.json();
+        if (response.ok) {
+            displayErrors(data);
+        } else {
+            throw new Error(data.error);
+        }
+}
+
+function displayErrors(data) {
+    let results = "";
+
+    let heading = `JSHint Results for ${data.file}`;
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}:</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+
+    document.getElementById("resultsModalTitle").innerText = heading; /* sets heading in modal */
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+}
 
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
@@ -15,9 +54,8 @@ async function getStatus(e) {
 
     if(response.ok) {
         displayStatus(data);
-    }
-    else {
-        throw new Error(data.error);  // descriptive message from JSON being returned
+    } else {
+    throw new Error(data.error);  // descriptive message from JSON being returned
     }
 }
 
@@ -31,3 +69,5 @@ function displayStatus(data) {
     document.getElementById("results-content").innerHTML = results;
     resultsModal.show();
 }
+
+//convert response to JSON & display it
