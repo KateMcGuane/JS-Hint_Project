452d5b41b1cf893bcbd3a9c5193ebee08fd2ca32
Kate McGuane <katemcguane@hotmail.com>
2024-06-06 11:48:50 +0000

    Add event listener for POST

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
-/* not actually using event listener for these lessons, but good habit to pass evnet into object handler */
+/* not actually using event listener for these lessons, but good habit to pass event into object handler */
+document.getElementById("submit").addEventListener("click", e => postForm(e));
 
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
